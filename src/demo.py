9e9a827991c13bfce6f2bc06fc01d8500eaf3658
randydapogi <randy.beros@gmail.com>
2026-02-07 15:53:29 +0800

    added test for class Point distance_to instance method in demo.py

--- src/demo.py
+++ src/demo.py
@@ -1,5 +1,20 @@
 from spatial import Point 
 
+# B.2.3
+# p = Point("A", 121.0, 14.6) 
+# print(p.id, p.lon, p.lat) 
 
-p = Point("A", 121.0, 14.6) 
-print(p.id, p.lon, p.lat)
+# B.3.3
+# q = Point("X", 999, 14) 
+# print(q.id, q.lon, q.lat)
+
+# B.5.4
+# p = Point("A", 121.0, 14.6) 
+# print(p.id, p.lon, p.lat) 
+# print(p.to_tuple())
+
+# Distance to Test
+p1 = Point("A", 121.0, 14.6) 
+p2 = Point("A", 121.4, 14.23)
+
+print(f"Point {p1.id} ({p1.lat}, {p1.lat}) is {p1.distance_to(p2)} meters to {p2.id} ({p2.lat}, {p2.lat}).")
