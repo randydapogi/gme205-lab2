6fc92e3d016521ab26afa47769b1b603bc78f522
randydapogi <randy.beros@gmail.com>
2026-02-07 15:37:53 +0800

    created a basic Point class

---
+++ src/demo.py
@@ -0,0 +1,5 @@
+from spatial import Point 
+
+
+p = Point("A", 121.0, 14.6) 
+print(p.id, p.lon, p.lat)
---
+++ src/spatial.py
@@ -0,0 +1,7 @@
+
+
+class Point: 
+    def __init__(self, id, lon, lat): 
+        self.id = id 
+        self.lon = lon 
+        self.lat = lat
